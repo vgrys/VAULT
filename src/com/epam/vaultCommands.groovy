e4abd98f625caf1756cdf44f1b1037bc7920ee0f
vgrys <vgrysster@gmail.com>
2017-09-11 18:08:47 +0300

    drg

--- src/com/epam/vaultCommands.groovy
+++ src/com/epam/vaultCommands.groovy
@@ -6,11 +6,11 @@ package com.epam
 @Grab('org.apache.commons:commons-math3:3.4.1') import org.apache.commons.math3.primes.Primes
 
 class vaultCommands implements Serializable {
-def print_command(int n) {
+ static  def print_command(int n) {
 
-    def config = [:]
-    // body()
-    println("\n$USER")
-    println(Primes.isPrime(n))
-}
+      def config = [:]
+      // body()
+      println("\n$USER")
+      println(Primes.isPrime(n))
+  }
 }
