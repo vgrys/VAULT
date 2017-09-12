376d6eb742ce2666dc434c8cf6c5f4d9095dec72
vgrys <vgrysster@gmail.com>
2017-09-12 13:11:25 +0300

    df

--- src/com/epam/Vault.groovy
+++ src/com/epam/Vault.groovy
@@ -1,13 +1,11 @@
 package com.epam
 
 @Grab('com.bettercloud:vault-java-driver:3.0.0')
-//import com.bettercloud.vault.VaultConfig
-import com.bettercloud.vault.Vault
-import com.epam.VaultConfig
+import com.bettercloud.vault.VaultConfig
 
 def get_credentials() {
 
-    def config = new VaultConfig
+    def config = new VaultConfig()
             .address("http://192.168.56.21:8200")
             .token("32dc6946-75cb-dbd7-e718-77b12be3a74b")
             .build();
