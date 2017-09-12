cb5330e60321671bd7b13074c217e648febffcc3
vgrys <vgrysster@gmail.com>
2017-09-12 11:27:23 +0300

    hj

--- src/com/epam/vaultCommands.groovy
+++ src/com/epam/vaultCommands.groovy
@@ -3,11 +3,11 @@
 package com.epam
 
 @Grapes([
-        @Grab(group = 'org.apache.commons:commons-math3:3.4.1'),
+        @Grab('org.apache.commons:commons-math3:3.4.1'),
         @GrabConfig( systemClassLoader=true )
 ])
 
-//@Grab('org.apache.commons:commons-math3:3.4.1') @GrabConfig(systemClassLoader=true)
+//@Grab('org.apache.commons:commons-math3:3.4.1'), @GrabConfig(systemClassLoader=true)
 import org.apache.commons.math3.primes.Primes
 
 class vaultCommands implements Serializable {
