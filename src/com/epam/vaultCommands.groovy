1984252cc357118b289725f91e66f68a3087dcd4
vgrys <vgrysster@gmail.com>
2017-09-12 11:33:05 +0300

    fgh

--- src/com/epam/vaultCommands.groovy
+++ src/com/epam/vaultCommands.groovy
@@ -2,20 +2,16 @@
 
 package com.epam
 
-@Grapes([
-        @Grab('org.apache.commons:commons-math3:3.4.1'),
-        @GrabConfig( systemClassLoader=true )
-])
 
-//@Grab('org.apache.commons:commons-math3:3.4.1'), @GrabConfig(systemClassLoader=true)
+@Grab('org.apache.commons:commons-math3:3.4.1')
+//@GrabConfig(systemClassLoader=true)
+
 import org.apache.commons.math3.primes.Primes
 
 class vaultCommands implements Serializable {
     def print_command(int n) {
 
-//      def config = [:]
-//       body()
-        ServiceLoader<Primes> loader = ServiceLoader.load(Primes.class);
+//        ServiceLoader<Primes> loader = ServiceLoader.load(Primes.class);
         println("Hello World")
         println(Primes.isPrime(n))
     }
