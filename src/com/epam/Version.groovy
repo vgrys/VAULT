a4640ecf4ebc706340980ca16468b3f576f40369
vgrys <vgrysster@gmail.com>
2017-09-12 10:36:15 +0300

    ft

---
+++ src/com/epam/Version.groovy
@@ -0,0 +1,144 @@
+package com.epam
+
+import com.cloudbees.groovy.cps.NonCPS
+
+@GrabResolver(name='nexus', root='https://nexus.terradatum.com/repository/public/')
+@Grab('com.github.zafarkhaja:java-semver:0.10-SNAPSHOT')
+
+/**
+ * @author rbellamy@terradatum.com
+ * @date 8/21/16
+ */
+class Version implements Serializable, Comparable {
+
+    private static final long serialVersionUID = -2008891377046871665L;
+    com.github.zafarkhaja.semver.Version delegate;
+
+    private def Version(com.github.zafarkhaja.semver.Version version) {
+        delegate = version
+    }
+
+    def Version(String versionString) {
+        delegate = com.github.zafarkhaja.semver.Version.valueOf(versionString)
+    }
+
+    void setDelegate(com.github.zafarkhaja.semver.Version version) {
+        this.delegate = version
+    }
+
+    boolean satisfies(String expr) {
+        return delegate.satisfies(expr)
+    }
+
+    Version incrementMajorVersion() {
+        return new Version(delegate.incrementMajorVersion())
+    }
+
+    Version incrementMajorVersion(String preRelease) {
+        return new Version(delegate.incrementMajorVersion(preRelease))
+    }
+
+    Version incrementMinorVersion() {
+        return new Version(delegate.incrementMinorVersion())
+    }
+
+    Version incrementMinorVersion(String preRelease) {
+        return new Version(delegate.incrementMinorVersion(preRelease))
+    }
+
+    Version incrementPatchVersion() {
+        return new Version(delegate.incrementPatchVersion())
+    }
+
+    Version incrementPatchVersion(String preRelease) {
+        return new Version(delegate.incrementPatchVersion(preRelease))
+    }
+
+    Version incrementPreReleaseVersion() {
+        return new Version(delegate.incrementPreReleaseVersion())
+    }
+
+    Version incrementBuildMetadata() {
+        return new Version(delegate.incrementBuildMetadata())
+    }
+
+    Version setPreReleaseVersion(String preRelease) {
+        return new Version(delegate.setPreReleaseVersion(preRelease))
+    }
+
+    Version setBuildMetadata(String build) {
+        return new Version(delegate.setBuildMetadata(build))
+    }
+
+    int getMajorVersion() {
+        return delegate.getMajorVersion()
+    }
+
+    int getMinorVersion() {
+        return delegate.getMinorVersion()
+    }
+
+    int getPatchVersion() {
+        return delegate.getPatchVersion()
+    }
+
+    String getNormalVersion() {
+        return delegate.getNormalVersion()
+    }
+
+    String getPreReleaseVersion() {
+        return delegate.getPreReleaseVersion()
+    }
+
+    String getBuildMetadata() {
+        return delegate.getBuildMetadata()
+    }
+
+    boolean greaterThan(Version other) {
+        return delegate.greaterThan(other.delegate)
+    }
+
+    boolean greaterThanOrEqualTo(Version other) {
+        return delegate.greaterThanOrEqualTo(other.delegate)
+    }
+
+    boolean lessThan(Version other) {
+        return delegate.lessThan(other.delegate)
+    }
+
+    boolean lessThanOrEqualTo(Version other) {
+        return delegate.lessThanOrEqualTo(other.delegate)
+    }
+
+    @Override
+    @NonCPS
+    boolean equals(Object other) {
+        return delegate == (other as Version).delegate
+    }
+
+    @Override
+    @NonCPS
+    int hashCode() {
+        return delegate.hashCode()
+    }
+
+    @Override
+    @NonCPS
+    String toString() {
+        return delegate.toString()
+    }
+
+    @Override
+    @NonCPS
+    int compareTo(Object other) {
+        return delegate <=> (other as Version).delegate
+    }
+
+    int compareWithBuildsTo(Version other) {
+        return delegate.compareWithBuildsTo(other.delegate)
+    }
+
+    static Version valueOf(String versionString) {
+        return new Version(versionString)
+    }
+}
--- vars/vault.groovy
+++ vars/vault.groovy
@@ -1,14 +1,34 @@
 #!/usr/bin/groovy
+import com.epam.Version
+
 // @Grapes(
 //     @Grab(group='com.bettercloud', module='vault-java-driver', version='3.0.0')
 // )
 
 import com.epam.vaultCommands
 
-def call(body) {
+//def call(body) {
+//
+//    body()
+//    def vc = new vaultCommands()
+//    vc.print_command(11)
+//    echo "done"
+//}
 
+def call(body) {
+    // evaluate the body block, and collect configuration into the object
+    def config = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
     body()
-    def vc = new vaultCommands()
-    vc.print_command(11)
-    echo "done"
+
+//    def flow = new TerradatumCommands()
+
+    Version version = config.version
+
+    Version newVersion = version
+
+    echo "Tagged version: ${newVersion}"
+
+    return newVersion
 }
