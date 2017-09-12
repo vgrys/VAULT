c185ee54ac7eb4e22106f9cffc7caa0bae1b66ad
vgrys <vgrysster@gmail.com>
2017-09-12 10:38:18 +0300

    gyuj

--- vars/vault.groovy
+++ vars/vault.groovy
@@ -21,14 +21,17 @@ def call(body) {
     body.resolveStrategy = Closure.DELEGATE_FIRST
     body.delegate = config
     body()
+        def vc = new vaultCommands()
+    vc.print_command(11)
+    echo "done"
 
 //    def flow = new TerradatumCommands()
 
-    Version version = config.version
-
-    Version newVersion = version
-
-    echo "Tagged version: ${newVersion}"
-
-    return newVersion
+//    Version version = config.version
+//
+//    Version newVersion = version
+//
+//    echo "Tagged version: ${newVersion}"
+//
+//    return newVersion
 }
