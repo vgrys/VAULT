9dddcede8fafd4a9f037248a8bb5e33a9e1cb381
vgrys <vgrysster@gmail.com>
2017-09-12 23:00:53 +0300

    sdfg

--- src/com/epam/MyVault.groovy
+++ src/com/epam/MyVault.groovy
@@ -19,8 +19,8 @@ static def get_credentials(IP, token) {
     final LogicalResponse response = vault.logical().read("secret/consul")
     final username = response.getData().get("username")
     final password = response.getData().get("password")
-    println(username)
-    println(password)
+    println username
+    println password
 
 
 //    def value = vault.logical()
