2ca22e0070ce779b1aeb8a29c026d221f58165ba
vgrys <vgrysster@gmail.com>
2017-09-13 14:43:49 +0300

    saed

--- src/com/epam/MyVault.groovy
+++ src/com/epam/MyVault.groovy
@@ -22,7 +22,7 @@ static def populate_credentials(ip, token, Backend) {
         final LogicalResponse response = vault.logical().read(Backend)
         final String username = response.getData().get("username")
         final String password = response.getData().get("password")
-        printf(username)
+        return username
 //    return "User is: $username \nPass is: $password"
 //    set_env("${service.toUpperCase()}_USER", username)
 //    set_env("${service.toUpperCase()}_PWD", password)
