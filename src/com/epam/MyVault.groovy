995e568ef8d8e06a1e6a10814f06304adeef0525
vgrys <vgrysster@gmail.com>
2017-09-13 14:26:22 +0300

    fsdf

--- src/com/epam/MyVault.groovy
+++ src/com/epam/MyVault.groovy
@@ -17,12 +17,16 @@ static def populate_credentials(ip, token, String environment, String service) {
 
     final Vault vault = new Vault(config)
 
-    final LogicalResponse response = vault.logical().read("secret/production/consul")
-    final username = response.getData().get('username')
-    final password = response.getData().get('password')
-    return "user is: $username \nPass is: $password"
+        final LogicalResponse response = vault.logical().read("secret/production/consul")
+        final username = response.getData().get("username")
+        final password = response.getData().get("password")
+        return "user is: $username \npass is: $password"
 //    set_env("${service.toUpperCase()}_USER", username)
 //    set_env("${service.toUpperCase()}_PWD", password)
 //    return "user is: (\"${service.toUpperCase()}_USER\", username) \npass is: (\"${service.toUpperCase()}_PWD\", password)"
+
+//    final  LogicalResponse response = vault.logical().read(Backend)
+//    final String username = response.getData().get("username")
+//    final String password = response.getData().get("password")
 
 }
