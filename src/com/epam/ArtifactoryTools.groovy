baefce367d1611a67bbcfc676de7d0a1c75a0dd5
vgrys <vgrysster@gmail.com>
2017-09-19 15:16:10 +0300

    fgh

--- src/com/epam/ArtifactoryTools.groovy
+++ src/com/epam/ArtifactoryTools.groovy
@@ -1,15 +1,17 @@
 package com.epam
 
 
-// vars/acme.groovy
-def setName(value) {
-    name = value
-}
-def getName() {
-    name
-}
-def caution(message) {
-    echo "Hello, ${name}! CAUTION: ${message}"
+class acme implements Serializable {
+    private String name
+    def setName(value) {
+        name = value
+    }
+    def getName() {
+        name
+    }
+    def caution(message) {
+        echo "Hello, ${name}! CAUTION: ${message}"
+    }
 }
 
 //def TIMESTAMP = new java.text.SimpleDateFormat('yyyyMMddHHmmss').format(new Date())
