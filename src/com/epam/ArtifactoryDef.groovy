43dec28f05be873fbf498e27b1572c251df00afb
vgrys <vgrysster@gmail.com>
2017-09-19 23:24:34 +0300

    dfg

--- src/com/epam/ArtifactoryDef.groovy
+++ src/com/epam/ArtifactoryDef.groovy
@@ -1,16 +1,29 @@
 #!/usr/bin/groovy
+//package ctc.ad.corp.cicd
+
 package com.epam
 
+@Grapes(
+        @Grab('com.bettercloud:vault-java-driver:3.0.0')
+)
 
-def call(body) {
-    body()
-    println("\n$USER")
+import com.bettercloud.vault.Vault
+import com.bettercloud.vault.VaultConfig
+import com.bettercloud.vault.response.LogicalResponse
+
+static def populate_credentials(env, vault_ip, token, String environment, String service) {
+
+    final VaultConfig config = new VaultConfig()
+            .address(vault_ip)
+            .token(token)
+            .build()
+
+    final Vault vault = new Vault(config)
+
+    final LogicalResponse response = vault.logical().read("secret/$environment/$service")
+    final String username = response.getData().get("username")
+    final String password = response.getData().get("password")
+
+    env.setProperty("${service.toUpperCase()}_USER", username)
+    env.setProperty("${service.toUpperCase()}_PWD", password)
 }
-
-
-//def TIMESTAMP = new java.text.SimpleDateFormat('yyyyMMddHHmmss').format(new Date())
-//
-//
-//def jobBaseName = "${env.JOB_NAME}".split('/')
-
-
