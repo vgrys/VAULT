c4995fb7d4808f618740549d34d480913f8a5d4d
vgrys <vgrysster@gmail.com>
2017-09-20 21:29:27 +0300

    ZIP created.

--- src/com/epam/ArtifactoryDef.groovy
+++ src/com/epam/ArtifactoryDef.groovy
@@ -1,19 +1,20 @@
 #!/usr/bin/groovy
 package com.epam
 
-
 //@Grapes(
 //        @Grab('org.jenkins-ci.plugins:pipeline-utility-steps:1.4.1')
 //)
 
-@Grapes(
-        @Grab('org.jfrog.artifactory.client:artifactory-java-client-services:2.3.5')
-)
+@Grapes([
+        @Grab('org.jfrog.artifactory.client:artifactory-java-client-services:2.3.5'),
+        @Grab('org.jenkins-ci.plugins:pipeline-utility-steps:1.4.1')
+])
 
+import org.jenkinsci.plugins.pipeline.utility.steps.zip.ZipStep
 
 import org.jfrog.artifactory.client.ArtifactoryClient
 import org.jfrog.artifactory.client.impl.ArtifactoryImpl
-
+import org.jfrog.artifactory.client.impl.UploadableArtifactImpl
 
 static def configure_artifactory(env, atifactory_ip, repository) {
     def TIMESTAMP = new java.text.SimpleDateFormat('yyyyMMddHHmmss').format(new Date())
@@ -22,19 +23,24 @@ static def configure_artifactory(env, atifactory_ip, repository) {
     def ArtifactoryUploadPath = "${env.JOB_NAME}/${env.BUILD_NUMBER}/"
     def ArtifactoryAddress = "${atifactory_ip}/artifactory/${repository}"
 
-//    def uploadSpec = """{
-//                            "files": [{
-//                                "pattern": "*.zip",
-//                                "target": "${ArtifactoryUploadPath}"
-//                             }]
-//                        }"""
+    def uploadSpec = """{
+                            "files": [{
+                                "pattern": "*.zip",
+                                "target": "${ArtifactoryUploadPath}"
+                             }]
+                        }"""
+
+
+        ZipStep step = new ZipStep("${projectName}-${TIMESTAMP}.zip");
+        step.setDir("");
+//        step.setGlob("**/*.zip");
+        step.setArchive(true);
 
 
 
-
-    ArtifactoryImpl artifactory = ArtifactoryClient.create("${ArtifactoryAddress}", "${env.ARTIFACTORY_USER}", "${env.ARTIFACTORY_PWD}")
-    java.io.File file = new java.io.File("*.zip")
-    File result = artifactory.repository("${repository}").upload("${ArtifactoryUploadPath}", file).doUpload()
+//    ArtifactoryImpl artifactory = ArtifactoryClient.create("${ArtifactoryAddress}", "${env.ARTIFACTORY_USER}", "${env.ARTIFACTORY_PWD}")
+//    java.io.File file = new java.io.File("*.zip")
+//    File result = artifactory.repository("${repository}").upload("${ArtifactoryUploadPath}", file).doUpload()
 
 //    def buildInfo = Artifactory.newBuildInfo()
 //    buildInfo.env.capture = true
---
+++ src/com/epam/ZipUnzip.groovy
@@ -0,0 +1,7 @@
+package com.epam
+
+@Grapes(
+        @Grab('org.jenkins-ci.plugins:pipeline-utility-steps:1.4.1')
+)
+
+
