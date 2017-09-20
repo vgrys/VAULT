9bfef620508409a59882cc15b8826c864484229e
vgrys <vgrysster@gmail.com>
2017-09-20 22:30:17 +0300

    sdfsdfsdf

--- src/com/epam/ArtifactoryDef.groovy
+++ src/com/epam/ArtifactoryDef.groovy
@@ -12,23 +12,7 @@ package com.epam
 import org.jenkinsci.plugins.pipeline.utility.steps.zip.ZipStep
 
 
-import hudson.model.Label;
-import hudson.model.Result;
-import hudson.model.Run;
-import jenkins.util.VirtualFile;
-import org.jenkinsci.plugins.workflow.cps.CpsFlowDefinition;
-import org.jenkinsci.plugins.workflow.job.WorkflowJob;
-import org.jenkinsci.plugins.workflow.job.WorkflowRun;
-import org.jenkinsci.plugins.workflow.steps.StepConfigTester;
-import org.junit.Before;
-import org.junit.Rule;
-import org.junit.Test;
-import org.jvnet.hudson.test.JenkinsRule;
 
-import java.io.IOException;
-import java.util.Scanner;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipInputStream;
 
 import org.jfrog.artifactory.client.ArtifactoryClient
 import org.jfrog.artifactory.client.impl.ArtifactoryImpl
