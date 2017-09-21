239e404e63b5384a3f371ef486eff42f6cd8bec6
vgrys <vgrysster@gmail.com>
2017-09-21 10:28:46 +0300

    ergegr

--- src/com/epam/ArtifactoryDef.groovy
+++ src/com/epam/ArtifactoryDef.groovy
@@ -21,14 +21,14 @@ static def configure_artifactory(env, atifactory_ip, repository) {
     def jobBaseName = "${env.JOB_NAME}".split('/')
     def projectName = "${jobBaseName[0]}"
     def ArtifactoryUploadPath = "${env.JOB_NAME}/${env.BUILD_NUMBER}/"
-    def ArtifactoryAddress = "${atifactory_ip}/artifactory/${repository}"
+    def ArtifactoryAddress = "${atifactory_ip}/artifactory/"
 
-    def uploadSpec = """{
-                            "files": [{
-                                "pattern": "*.zip",
-                                "target": "${ArtifactoryUploadPath}"
-                             }]
-                        }"""
+//    def uploadSpec = """{
+//                            "files": [{
+//                                "pattern": "*.zip",
+//                                "target": "${ArtifactoryUploadPath}"
+//                             }]
+//                        }"""
 
     ZipStep step = new ZipStep("TEMP123.zip")
             step.setDir('/home/vagrant/TEST/')
@@ -36,27 +36,11 @@ static def configure_artifactory(env, atifactory_ip, repository) {
 
     Artifactory artifactory = ArtifactoryClient.create("${ArtifactoryAddress}", "${env.ARTIFACTORY_USER}", "${env.ARTIFACTORY_PWD}")
     java.io.File file = new java.io.File("${env.WORKSPACE}/TEST.zip");
-    return file.getPath()
-    print(file.getPath())
     File result = artifactory.repository("${repository}").upload("${ArtifactoryUploadPath}", file).doUpload()
 
     env.setProperty("${"TIMESTAMP"}", TIMESTAMP)
     env.setProperty("${"PROJECT_NAME"}", projectName)
     env.setProperty("${"ARTIFACTORY_ADDRESS"}", ArtifactoryAddress)
-    env.setProperty("${"UPLOAD_SPEC"}", uploadSpec)
+//    env.setProperty("${"UPLOAD_SPEC"}", uploadSpec)
     return result.getDownloadUri()
 }
-
-//    ArtifactoryImpl artifactory = ArtifactoryClient.create("${ArtifactoryAddress}", "${env.ARTIFACTORY_USER}", "${env.ARTIFACTORY_PWD}")
-//    java.io.File file = new java.io.File("*.zip")
-//    File result = artifactory.repository("${repository}").upload("${ArtifactoryUploadPath}", file).doUpload()
-
-//    def buildInfo = Artifactory.newBuildInfo()
-//    buildInfo.env.capture = true
-//    def done = artifactory.upload("${env.UPLOAD_SPEC}")
-
-
-//def ArtifactoryServer = Artifactory.newServer("${env.ARTIFACTORY_ADDRESS}", "${env.ARTIFACTORY_USER}", "${env.ARTIFACTORY_PWD}")
-//def buildInfo = Artifactory.newBuildInfo()
-//buildInfo.env.capture = true
-//ArtifactoryServer.upload("${env.UPLOAD_SPEC}")
