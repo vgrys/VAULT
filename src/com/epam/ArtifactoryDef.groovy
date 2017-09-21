2efdbb0c5481a63f3a46f7d4b38e26a7534bcffd
vgrys <vgrysster@gmail.com>
2017-09-21 11:04:47 +0300

    bn

--- src/com/epam/ArtifactoryDef.groovy
+++ src/com/epam/ArtifactoryDef.groovy
@@ -20,8 +20,8 @@ static def configure_artifactory(env, atifactory_ip, repository) {
     def TIMESTAMP = new java.text.SimpleDateFormat('yyyyMMddHHmmss').format(new Date())
     def jobBaseName = "${env.JOB_NAME}".split('/')
     def projectName = "${jobBaseName[0]}"
-    def ZIPNAME = "${projectName}-${TIMESTAMP}.zip"
-    def ArtifactoryUploadPath = "${env.JOB_NAME}/${env.BUILD_NUMBER}/${ZIPNAME}"
+    java.io.File file = new java.io.File("${env.WORKSPACE}/TEST.zip")
+    def ArtifactoryUploadPath = "${env.JOB_NAME}/${env.BUILD_NUMBER}/${file.getName()}"
     def ArtifactoryAddress = "${atifactory_ip}/artifactory/"
 
 
