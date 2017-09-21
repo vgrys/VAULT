a09100d80a7995911d4e1f443d738ae2f5048bcc
vgrys <vgrysster@gmail.com>
2017-09-21 11:01:48 +0300

    fgh

--- src/com/epam/ArtifactoryDef.groovy
+++ src/com/epam/ArtifactoryDef.groovy
@@ -30,7 +30,7 @@ static def configure_artifactory(env, atifactory_ip, repository) {
             step.setArchive(true)
 
     Artifactory artifactory = ArtifactoryClient.create("${ArtifactoryAddress}", "${env.ARTIFACTORY_USER}", "${env.ARTIFACTORY_PWD}")
-    java.io.File file = new java.io.File("${env.WORKSPACE}/${ZIPNAME}")
+    java.io.File file = new java.io.File("${env.WORKSPACE}/TEST.zip")
     File result = artifactory.repository("${repository}").upload("${ArtifactoryUploadPath}", file).doUpload()
 
     env.setProperty("${"TIMESTAMP"}", TIMESTAMP)
