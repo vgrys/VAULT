6a774fa6b248831373ca5b7e9d035c07501bc074
vgrys <vgrysster@gmail.com>
2017-09-21 11:58:25 +0300

    sdf

--- src/com/epam/ArtifactoryDef.groovy
+++ src/com/epam/ArtifactoryDef.groovy
@@ -3,8 +3,8 @@ package com.epam
 
 
 @Grapes([
-//        @Grab(group='org.jfrog.artifactory.client', module='artifactory-java-client-services', version='2.5.2'),
-        @Grab(group='org.jfrog.artifactory.client', module='artifactory-java-client-api', version='2.5.2'),
+        @Grab(group='org.jfrog.artifactory.client', module='artifactory-java-client-services', version='2.5.2'),
+//        @Grab(group='org.jfrog.artifactory.client', module='artifactory-java-client-api', version='2.5.2'),
 //        @Grab(group='org.codehaus.groovy', module='groovy-xml', version='2.3.2'),
 //        @Grab(group='commons-io', module='commons-io', version='2.5')
 ])
