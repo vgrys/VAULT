dd2d0524ce8c37d0315a448b0e3419a3b39e20c8
vgrys <vgrysster@gmail.com>
2017-09-21 10:59:50 +0300

    Artifactory with no pluging is now should work

--- src/com/epam/ArtifactoryDef.groovy
+++ src/com/epam/ArtifactoryDef.groovy
@@ -20,7 +20,7 @@ static def configure_artifactory(env, atifactory_ip, repository) {
     def TIMESTAMP = new java.text.SimpleDateFormat('yyyyMMddHHmmss').format(new Date())
     def jobBaseName = "${env.JOB_NAME}".split('/')
     def projectName = "${jobBaseName[0]}"
-    def ZIPNAME = "${env.BUILD_NUMBER}/${projectName}-${TIMESTAMP}.zip"
+    def ZIPNAME = "${projectName}-${TIMESTAMP}.zip"
     def ArtifactoryUploadPath = "${env.JOB_NAME}/${env.BUILD_NUMBER}/${ZIPNAME}"
     def ArtifactoryAddress = "${atifactory_ip}/artifactory/"
 
