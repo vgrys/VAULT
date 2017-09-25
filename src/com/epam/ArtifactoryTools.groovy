f7b28596adf64ef90903c7c3127dc69f71cd80dc
vgrys <vgrysster@gmail.com>
2017-09-25 12:26:44 +0300

    dfg

--- src/com/epam/ArtifactoryTools.groovy
+++ src/com/epam/ArtifactoryTools.groovy
@@ -5,6 +5,7 @@ package com.epam
         @Grab(group='commons-io', module='commons-io', version='2.5'),
         @Grab(group='org.jfrog.artifactory.client', module='artifactory-java-client-services', version='2.5.2'),
         @Grab(group='org.jfrog.artifactory.client', module='artifactory-java-client-api', version='2.5.2'),
+        @Grab(group='com.fasterxml.jackson.core', module='jackson-core', version='2.9.1'),
         @GrabExclude(group='org.codehaus.groovy', module='groovy-xml'),
         @GrabExclude(group='xerces', module='xercesImpl')
 ])
@@ -17,8 +18,6 @@ static def upload(env, atifactory_ip, repository, String artifactPath, ARTIFACTO
 
     java.io.File artifact = new java.io.File(artifactPath)
     def ArtifactoryUploadPath = "${env.JOB_NAME}/${env.BUILD_NUMBER}/${artifact.getName()}"
-
-//    Artifactory artifactory = ArtifactoryClientBuilder.create()
 
     Artifactory artifactory = ArtifactoryClientBuilder.create()
                 .setUrl("${atifactory_ip}/artifactory/")
