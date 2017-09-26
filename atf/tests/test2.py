4a5cba81cda47ef16d6fb772c9ac080ec1a166e9
vgrys <vgrysster@gmail.com>
2017-09-26 09:54:18 +0300

    added one more file with tests

--- atf/tests/test2.py
+++ atf/tests/test2.py
@@ -10,8 +10,21 @@ class SimpleTest(unittest.TestCase):
     def test_skipped(self):
         self.fail("shouldn't happen")
 
+    def test_skipped_again(self):
+        self.fail("shouldn't happen anything")
+
+    def test_skipped_and_again(self):
+        self.fail("shouldn't happen anything again")
+
     def test_pass(self):
         self.assertEqual(10, 7 + 3)
+
+    def test_pass_again(self):
+        self.assertEqual(15, 10 + 5)
+
+    def test_pass_and_again(self):
+        self.assertEqual(20, 10 + 10)
+
 
     # def test_fail(self):
     #     self.assertEqual(11, 7 + 3)
