26deb25cc102054e08515586f6d55543859e406d
vgrys <vgrysster@gmail.com>
2017-09-26 13:29:02 +0300

    sdfg

--- src/com/epam/ZipTools.groovy
+++ src/com/epam/ZipTools.groovy
@@ -12,7 +12,7 @@ static def bundle(env, incl, excl) {
 
 //    (new AntBuilder()).zip(destfile: zipFilePath, basedir: sourceFolder)
     new AntBuilder().zip(destfile: zipFilePath, basedir: sourceFolder,
-            include: "${incl}",
-            exclude: "${excl}")
+            includes: "${incl}",
+            excludes: "${excl}")
     return zipFilePath
 }
