aae12735c59b73a45d1f65eee959e0fe898e47e7
vgrys <vgrysster@gmail.com>
2017-09-27 13:27:51 +0300

    fgh

--- src/com/epam/ZipTools.groovy
+++ src/com/epam/ZipTools.groovy
@@ -4,15 +4,15 @@ package com.epam
 import java.text.SimpleDateFormat
 
 static def bundle(env, incl, excl) {
-    def filesToInclude = [incl]
-    def filesToExclude = ['**/*.groovy', '**/tests/*', '**/*__init__*']
+//    def filesToInclude = [incl]
+//    def filesToExclude = ['**/*.groovy', '**/tests/*', '**/*__init__*']
     def TIMESTAMP = new SimpleDateFormat('yyyyMMddHHmmss').format(new Date())
     def jobBaseName = "${env.JOB_NAME}".split('/')
     def projectName = "${jobBaseName[0]}"
     String zipFilePath = "${env.WORKSPACE}/${projectName}_${TIMESTAMP}.zip"
     String sourceFolder = "${env.WORKSPACE}"
 
-    (new AntBuilder()).zip(destfile: zipFilePath, basedir: sourceFolder)
+    new AntBuilder().zip(destfile: zipFilePath, basedir: sourceFolder)
 //    new AntBuilder().zip(destfile: zipFilePath,
 //            basedir: sourceFolder,
 //            includes: filesToInclude.join(' '),
