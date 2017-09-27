8a945233fc190db0cc62e4cc51f517d59f92eb4b
vgrys <vgrysster@gmail.com>
2017-09-27 13:24:15 +0300

    dfg

--- src/com/epam/ZipTools.groovy
+++ src/com/epam/ZipTools.groovy
@@ -12,9 +12,9 @@ static def bundle(env, incl, excl) {
     String zipFilePath = "${env.WORKSPACE}/${projectName}_${TIMESTAMP}.zip"
     String sourceFolder = "${env.WORKSPACE}"
 
-//    (new AntBuilder()).zip(destfile: zipFilePath, basedir: sourceFolder)
-    new AntBuilder().zip(destfile: zipFilePath,
-            basedir: sourceFolder)
+    (new AntBuilder()).zip(destfile: zipFilePath, basedir: sourceFolder)
+//    new AntBuilder().zip(destfile: zipFilePath,
+//            basedir: sourceFolder,
 //            includes: filesToInclude.join(' '),
 //            excludes: filesToExclude.join(' '))
 
