d698dcb94c846d76529bf427b786410a24fb70c3
vgrys <vgrysster@gmail.com>
2017-11-01 22:02:34 +0200

    tests

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='atf',
+    version='0.0.1',
+    packages=['atf'],
+    url='canadiantire.com',
+    license='',
+    author='IDEA+',
+    author_email='',
+    description='IDEA+ Automated Testing Framework'
+)
