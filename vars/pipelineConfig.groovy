f5be3d07859cbeb2f678d66533cff1417e2128e0
vgrys <vgrysster@gmail.com>
2017-11-02 15:50:55 +0200

    tests

---
+++ vars/pipelineConfig.groovy
@@ -0,0 +1,61 @@
+#!/usr/bin/groovy
+
+def pad(str) {
+    return "********** ${str} ***********"
+}
+
+def createProjectBundle(sourceFolder) {
+    def zip = new ZipTools()
+    def bundlePath = zip.bundle(env, sourceFolder, ['.git', '.gitignore'])
+    echo "created an archive $bundlePath"
+}
+
+
+def runATFCommand(targetHost, user, projectName, command) {
+    String commandToRun = "cd /home/${user}/${projectName}; source ./ATFVENV/bin/activate; ${command}"
+    sh sshCli(targetHost, commandToRun)
+}
+
+def sshCli(host, commandToRun) {
+    return "ssh -o StrictHostKeyChecking=no ${host} /bin/bash -c '\"${commandToRun}\"'"
+}
+
+def executeAnsible(ansibleCommand) {
+    withCredentials([usernamePassword(credentialsId: 'arifactoryID', usernameVariable: 'artifactory_user', passwordVariable: 'artifactory_pwd')]) {
+        dir("${WORKSPACE}/ansible") {
+            sh ansibleCommand
+        }
+    }
+}
+
+def reportGitParams() {
+    echo "Git Origin: ${env.GIT_ORIGIN}, Git User: ${env.GIT_USER}, Git Project: ${env.GIT_PROJECT}, Git Branch: ${env.GIT_BRANCH}, Git Repo: ${env.GIT_REPO}, Git Feature Name (optional): ${env.GIT_FEATURE_NAME}"
+}
+
+def ansible(command) {
+    return "ansible-playbook --extra-vars 'server=${targetGroup} user=artifactory_user password=artifactory_pwd ${command}"
+}
+
+def runDeployATF(String artifactoryRepo, String artifactoryUrl, String atfVersion, String projectName) {
+    withCredentials([usernamePassword(credentialsId: 'artifactoryIDVG', usernameVariable: 'artifactory_user', passwordVariable: 'artifactory_pwd')]) {
+        sh "cp $WORKSPACE/ci-cd-framework/requirements.txt $WORKSPACE/requirements.txt"
+        withCredentials([file(credentialsId: 'atf-config', variable: 'atfConf')]) {
+            dir("${WORKSPACE}/ci-cd-framework/ansible") {
+                sh ansible("artifactoryRepo=${artifactoryRepo} artifactoryUrl=${artifactoryUrl} atfVersion=${atfVersion} projectName=${projectName} workspace=${WORKSPACE} atfConf=${atfConf}' ATFDeployment.yml")
+            }
+        }
+    }
+}
+
+def runDeployProject(artifactoryUrl, artifactoryRepo, projectVersion, projectName) {
+    def cmd = ansible("artifactoryUrl=${artifactoryUrl} artifactoryRepo=${artifactoryRepo} projectVersion=${projectVersion} projectName=${projectName} workspace=${WORKSPACE}' projectDeployment.yml")
+    executeAnsible(cmd)
+}
+
+
+def runProjectCleanup(projectName) {
+    cmd = ansible("projectName=${projectName}' projectCleanup.yml")
+    executeAnsible(cmd)
+}
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
