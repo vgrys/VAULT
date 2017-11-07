665ca1ed98c4913c352445092b05437f88f2f5fb
vgrys <vgrysster@gmail.com>
2017-11-07 12:38:51 +0200

    tests

--- src/com/epam/ArtifactoryToolsPlugin.groovy
+++ src/com/epam/ArtifactoryToolsPlugin.groovy
@@ -1,33 +1,6 @@
 #!/usr/bin/groovy
 package com.epam.ArtifactoryToolsPlugin
 
-//def static artifactoryConfig(env, repository, String atfArchivePath, String projectArchivePath, atf_version, project_name, project_version) {
-//
-//    def branchDirs = [
-//        'develop': 'develop',
-//        'master': 'stable',
-//        'release': 'release'
-//    ]
-//
-//    def dirName = branchDirs.get(env.GIT_BRANCH_TYPE, '')
-//    if (dirName != '') {
-//        GString artifactoryATFPath = "artifactory/${repository}/atf/${dirName}/"
-//        GString artifactoryProjectPath = "artifactory/${repository}/${project_name}/${project_version}/"
-//
-//
-//        env.uploadSpec = """{
-//                    "files": [{
-//                        "pattern": "${atfArchivePath}",
-//                        "target": "${artifactoryATFPath}/"
-//                    },
-//                    {
-//                        "pattern": "${projectArchivePath}",
-//                        "target": "${artifactoryProjectPath}/${project_name}-${project_version}.tgz"
-//                    }]
-//                 }"""
-//    }
-//}
-
 def static artifactoryConfig(env, repository, String archive, name, version) {
 
     env.uploadSpec = """{
---
+++ vars/artifactoryTools.groovy
@@ -0,0 +1,43 @@
+#!/usr/bin/groovy
+
+def static artifactoryConfig(repository, String archive, name, version) {
+
+    env.uploadSpec = """{
+                    "files": [{
+                        "pattern": "${archive}",
+                        "target": "artifactory/${repository}/${name}/${version}/"
+                    }]
+                    }"""
+}
+
+def static artifactoryATFConfig(repository, String archive) {
+    def branchDirs = [
+            'develop': 'develop',
+            'master' : 'stable',
+            'release': 'release'
+    ]
+    def dirName = branchDirs.get(env.GIT_BRANCH_TYPE, '')
+    if (dirName != '') {
+        artifactoryConfig(repository, archive, "atf", dirName)
+    }
+}
+
+def static artifactoryProjectConfig(repository, String archive, name, version) {
+    artifactoryConfig(repository, archive, name, version)
+}
+
+def ATFUpload (artifactoryUrl, artifactoryRepo) {
+    GString atfArchivePath = "${WORKSPACE}/dist/*.tar.gz"
+    def server = Artifactory.newServer url: "${artifactoryUrl}", credentialsId: 'arifactoryID'
+    def artifactory = new artifactoryTools()
+    artifactory.artifactoryATFConfig(artifactoryRepo, atfArchivePath)
+    server.upload(env.uploadSpec)
+}
+
+def projectUpload (artifactoryUrl, artifactoryRepo, projectName, projectVersion) {
+    GString projectArchivePath = "${env.WORKSPACE}/*.tgz"
+    def server = Artifactory.newServer url: "${artifactoryUrl}", credentialsId: 'arifactoryID'
+    def artifactory = new artifactoryTools()
+    artifactory.artifactoryProjectConfig(artifactoryRepo, projectArchivePath, projectName, projectVersion)
+    server.upload(env.uploadSpec)
+}
