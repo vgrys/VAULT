ab562100019b942dee2ff627670721b90a597924
vgrys <vgrysster@gmail.com>
2017-11-07 12:04:50 +0200

    tests

--- src/com/epam/ArtifactoryToolsPlugin.groovy
+++ src/com/epam/ArtifactoryToolsPlugin.groovy
@@ -1,33 +1,56 @@
 #!/usr/bin/groovy
 package com.epam.ArtifactoryToolsPlugin
 
+//def static artifactoryConfig(env, repository, String atfArchivePath, String projectArchivePath, atf_version, project_name, project_version) {
+//
+//    def branchDirs = [
+//        'develop': 'develop',
+//        'master': 'stable',
+//        'release': 'release'
+//    ]
+//
+//    def dirName = branchDirs.get(env.GIT_BRANCH_TYPE, '')
+//    if (dirName != '') {
+//        GString artifactoryATFPath = "artifactory/${repository}/atf/${dirName}/"
+//        GString artifactoryProjectPath = "artifactory/${repository}/${project_name}/${project_version}/"
+//
+//
+//        env.uploadSpec = """{
+//                    "files": [{
+//                        "pattern": "${atfArchivePath}",
+//                        "target": "${artifactoryATFPath}/"
+//                    },
+//                    {
+//                        "pattern": "${projectArchivePath}",
+//                        "target": "${artifactoryProjectPath}/${project_name}-${project_version}.tgz"
+//                    }]
+//                 }"""
+//    }
+//}
 
-def static artifactoryConfig(env, repository, String atfArchivePath, String projectArchivePath, atf_version, project_name, project_version) {
 
-    def branchDirs = [
-        'develop': 'develop',
-        'master': 'stable',
-        'release': 'release'
-    ]
+def static artifactoryConfig(env, repository, String archive, name, version) {
 
-    def dirName = branchDirs.get(env.GIT_BRANCH_TYPE, '')
-    if (dirName != '') {
-        GString artifactoryATFPath = "artifactory/${repository}/atf/${dirName}/"
-
-    }
-    GString artifactoryProjectPath = "artifactory/${repository}/${project_name}/${project_version}/"
     env.uploadSpec = """{
                     "files": [{
-                        "pattern": "${atfArchivePath}",
-                        "target": "${artifactoryATFPath}/"
-                    },
-                    {
-                        "pattern": "${projectArchivePath}",
-                        "target": "${artifactoryProjectPath}/${project_name}-${project_version}.tgz"
-                    }]
+                        "pattern": "${archive}",
+                        "target": "artifactory/${repository}/${name}/${version}/"
+                    }
                  }"""
 }
 
-//when built from master - deploy to atf/stable/atf-*tar.gz
-//when built from develop - deploy to atf/develop/atf-*tar.gz
-//when built from release - deploy to atf/release/atf-*tar.gz (each release will upgrade version so files will stay unique)
+def static artifactoryATFConfig(env, repository, String archive) {
+    def branchDirs = [
+            'develop': 'develop',
+            'master' : 'stable',
+            'release': 'release'
+    ]
+    def dirName = branchDirs.get(env.GIT_BRANCH_TYPE, '')
+    if (dirName != '') {
+        artifactoryConfig(env, repository, archive, "atf", branchDirs)
+    }
+}
+
+def static artifactoryProjectConfig(env, repository, String archive, name, version) {
+    artifactoryConfig(env, repository, archive, name, version)
+}
