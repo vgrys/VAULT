b27c7f5b267a2d85a1ba84038844ad604c2c8c7d
vgrys <vgrysster@gmail.com>
2017-11-08 11:37:29 +0200

    tests

--- vars/ZipTools.groovy
+++ vars/ZipTools.groovy
@@ -1,3 +1,8 @@
+#!/usr/bin/groovy
+
+def extractAnsible(frameworkName, frameworkVersion) {
+    sh "tar -xzf ${env.WORKSPACE}/ansible//${frameworkName}/${frameworkVersion}/${frameworkName}-${frameworkVersion}.tgz -C ${env.WORKSPACE}/ansible/"
+}
 
 def bundle(sourceFolder, excludes, bundleName = '') {
 
@@ -18,8 +23,4 @@ def bundle(sourceFolder, excludes, bundleName = '') {
     sh "cd ${sourceFolder} && tar -zcf ${archhiveFilePath} ${excludeParameters} * "
 
     return bundleName
-}
-
-def static extractAnsible(frameworkName, frameworkVersion) {
-    sh "tar -xzf ${env.WORKSPACE}/ansible//${frameworkName}/${frameworkVersion}/${frameworkName}-${frameworkVersion}.tgz -C ${env.WORKSPACE}/ansible/"
 }
