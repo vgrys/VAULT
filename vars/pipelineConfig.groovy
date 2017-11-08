1b3a28a682fb1af54757339fea64216fc5936bb2
vgrys <vgrysster@gmail.com>
2017-11-08 13:08:09 +0200

    tests

--- vars/pipelineConfig.groovy
+++ vars/pipelineConfig.groovy
@@ -37,10 +37,9 @@ def static ansible(command, targetGroup) {
 
 def runDeployATF(String artifactoryRepo, String artifactoryUrl, String atfVersion, String release, String projectName, String targetGroup) {
     withCredentials([usernamePassword(credentialsId: 'arifactoryID', usernameVariable: 'artifactory_user', passwordVariable: 'artifactory_pwd')]) {
-        sh "cp ${env.WORKSPACE}/requirements.txt ${env.WORKSPACE}/requirements-new.txt"
         withCredentials([file(credentialsId: 'zeph', variable: 'zephCred')]) {
             dir("${env.WORKSPACE}/ansible") {
-                sh ansible("artifactoryRepo=${artifactoryRepo} artifactoryUrl=${artifactoryUrl} atfVersion=${atfVersion} atfRelease=${release} projectName=${projectName} workspace=${WORKSPACE} zephCred=${zephCred}' ATFDeployment.yml", targetGroup)
+                sh ansible("artifactoryUrl=${artifactoryUrl} artifactoryRepo=${artifactoryRepo} atfVersion=${atfVersion} atfRelease=${release} projectName=${projectName} workspace=${WORKSPACE} zephCred=${zephCred}' ATFDeployment.yml", targetGroup)
             }
         }
     }
