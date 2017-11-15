5ad37afc8ea52f0fd2b08d378c8479c73967c431
vgrys <vgrysster@gmail.com>
2017-11-15 19:11:14 +0200

    tests

--- vars/SharedConfiguration.groovy
+++ vars/SharedConfiguration.groovy
@@ -2,18 +2,16 @@
 import groovy.transform.NotYetImplemented
 
 class SharedConfiguration implements Serializable {
-    def getDefault() {
-        return [
-                jenkinsSlave1  : 'flex1',
-                jenkinsSlave2  : 'flex',
-                sshKeyId       : 'SSH-KEY',
-                artifactoryId  : 'arifactoryID',
-                artifactoryRepo: 'bigdata-dss-automation',
-                artifactoryUrl : 'http://192.168.56.105:8081'
-        ]
-    }
+    def getDefault = [
+            jenkinsSlave1  : 'flex1',
+            jenkinsSlave2  : 'flex',
+            sshKeyId       : 'SSH-KEY',
+            artifactoryId  : 'arifactoryID',
+            artifactoryRepo: 'bigdata-dss-automation',
+            artifactoryUrl : 'http://192.168.56.105:8081'
+    ]
 
-    def get(consulURL=null) {
+    def get(consulURL = null) {
         if (consulURL) {
             throw NotYetImplemented
         }
