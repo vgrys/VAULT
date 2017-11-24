1f48b85feb04ef2aaebe4f7f214d60b9746e2661
vgrys <vgrysster@gmail.com>
2017-11-24 14:23:30 +0200

    tests

--- vars/nifi.groovy
+++ vars/nifi.groovy
@@ -7,6 +7,6 @@ def get (URL, process, id) {
 //curl -X GET 192.168.56.105:8088/nifi-api/flow/process-groups/e96237ab-015f-1000-d7ee-a0ea33f79a1e
 
 def post (URL, process, rootID, name) {
-sh "curl -H \"Content-Type: application/json\" -X POST -d '{\"revision\":{\"version\":0},\"component\":{\"name\":\"test123}}' "${URL}"/nifi-api/"${process}"/"${rootID}"/process-groups"
+sh "curl -H \"Content-Type: application/json\" -X POST -d '{\"revision\":{\"version\":0},\"component\":{\"name\":\"test123\"}}' "${URL}"/nifi-api/"${process}"/"${rootID}"/process-groups"
 //    sh 'curl -H "Content-Type: application/json" -X POST -d '{"revision"':'{"clientId"':'${clientId}',"version":0},"component":{"name":'${name}'}}' "${URL}"/nifi-api/"${process}"/"${rootID}"/"${process}"'
 }
