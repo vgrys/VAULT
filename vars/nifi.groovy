47a3db0b06c8495155b44cd808c464539f03db27
vgrys <vgrysster@gmail.com>
2017-11-29 15:33:49 +0200

    tests

--- vars/nifi.groovy
+++ vars/nifi.groovy
@@ -23,7 +23,7 @@ def uploadTemplate (nifiURL, process, nifiRootID, nifiClientID, groupFromJenkins
     def output = readFile('result').trim()
     def result = new XmlSlurper().parseText("${output}")
 //    assert result instanceof groovy.util.slurpersupport.GPathResult
-    echo "Name of the group is: '${result.templateEntity.template}'"
+    echo "Name of the group is: '${result.templateEntity}'"
 
 }
 
