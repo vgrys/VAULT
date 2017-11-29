582ddc7ac89c0c66229f1f3a24b63f0f54c28c95
vgrys <vgrysster@gmail.com>
2017-11-29 12:39:00 +0200

    tests

--- vars/nifi.groovy
+++ vars/nifi.groovy
@@ -3,11 +3,10 @@
 def get (URL, process, id) {
     sh "curl -X GET ${URL}/nifi-api/${process}/${id} > result"
     def output = readFile('result').trim()
-    echo "output=$output"
     echo "********** IN DSS ********************"
-//    echo "result is: ${result}"
 
-    return result
+    echo "output=$output"
+
 }
 
 //curl -X GET 192.168.56.105:8088/nifi-api/flow/process-groups/e96237ab-015f-1000-d7ee-a0ea33f79a1e
