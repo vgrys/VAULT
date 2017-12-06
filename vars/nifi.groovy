222ef79c9e30e7d227b0bdd51c7aca5ecbff338c
vgrys <vgrysster@gmail.com>
2017-12-06 17:32:16 +0200

    tests

--- vars/nifi.groovy
+++ vars/nifi.groovy
@@ -17,13 +17,13 @@ def getInfo (URL, process, id) {
 }
 
 def getInfoConnection (URL, process, id) {
-    sh "curl -X GET ${URL}/nifi-api/${process}/${id} > result"
+    sh "curl -X GET ${URL}/nifi-api/flow/${process}/${id} > result"
     def output = readFile('result').trim()
     echo "********** IN DSS ********************"
     def sluper = new JsonSlurper()
     def result = sluper.parseText("${output}")
     echo "Group ID is: '${result.component.id}'"
-    echo " Group name is: '${result.component.name}'"
+    echo " Group name is: '${result.processGroupFlow.breadcrumb.breadcrumb.name}'"
     echo "connections is: '${result.component.connections}'"
 //    echo "revision version is: '${result.revision.version}'"
 //    echo "parentGroupId is: '${result.component.parentGroupId}'"
