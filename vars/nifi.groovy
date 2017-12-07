962da56b1ebe57e0cf332e636338816dedefe59c
vgrys <vgrysster@gmail.com>
2017-12-07 15:39:31 +0200

    tests

--- vars/nifi.groovy
+++ vars/nifi.groovy
@@ -18,7 +18,7 @@ def uploadTemplate(URL) {
     List list = []
 
     File dir = new File("${env.WORKSPACE}/nifi")
-    dir.eachFileRecurse(FileType.FILES, println(it.name)) { file ->
+    dir.eachFileRecurse(FileType.FILES, {list1 << it.name }, println(list1)) { file ->
     println(file)
         list << file
     }
