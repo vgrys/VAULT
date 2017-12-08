f1d9abd7ee91b89c05f7dd9fcf05a965c8e56f7f
vgrys <vgrysster@gmail.com>
2017-12-08 16:38:16 +0200

    tests

--- vars/nifi.groovy
+++ vars/nifi.groovy
@@ -1,13 +1,19 @@
 #!/usr/bin/groovy
 import groovy.json.JsonSlurper
+import groovyjarjarantlr.collections.List
 
 def call(URL) {
     try {
         echo "********* Upload templates to the NiFi ************"
         def array = "ls -f ${env.WORKSPACE}/nifi".execute().text.trim().toString().split()
-        for (String item : array) {
-            print(item)
-        }
+        List list = Arrays.asList(array)
+        echo list
+        list.remove('.')
+        echo list
+
+//        for (String item : array) {
+//            print(item)
+//        }
 //        uploadTemplate(URL)
 //        createWorkspace(URL)
 //        createProcesGroups(URL)
