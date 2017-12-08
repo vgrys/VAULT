6136d4f349f729a47e3db70ca6059e321c38cbed
vgrys <vgrysster@gmail.com>
2017-12-08 11:22:49 +0200

    tests

--- vars/nifi.groovy
+++ vars/nifi.groovy
@@ -27,7 +27,8 @@ def uploadTemplate(URL, env) {
         def result = new XmlSlurper().parseText("${output}")
         echo "Name of the template is: '${result.template.name}'"
         echo "ID of the template is: '${result.template.id}'"
-        listTemplate << ["${result.template.id}",]
+        listTemplate << result.template.id
+        result = null
 //        env.templateId << "${result.template.id}"
 //        println(env.templateId)
 //        result.template.id.each {
