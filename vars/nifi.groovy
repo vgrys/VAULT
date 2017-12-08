d6dca5d0ab6d9730a6990021aa8fb239b2fcf15d
vgrys <vgrysster@gmail.com>
2017-12-08 11:53:19 +0200

    tests

--- vars/nifi.groovy
+++ vars/nifi.groovy
@@ -29,11 +29,8 @@ def uploadTemplate(URL, env) {
         }
     }
     sh "cat ${fileResult}"
-    def newList = readFile("${fileResult}").trim()
+    def newList = readFile("${fileResult}").trim().replace(" ", ",")
     print(newList)
-    def temp = newList.join(",")
-    print(newList)
-    println(temp)
     echo "End of uploadTemplate"
 }
 
