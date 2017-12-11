0c55d98a3f1ce66dee2eae5fea61d8cda1113d71
vgrys <vgrysster@gmail.com>
2017-12-11 14:04:51 +0200

    tests

--- vars/nifi.groovy
+++ vars/nifi.groovy
@@ -33,7 +33,7 @@ def uploadTemplate(URL) {
     sh 'cat shellOutput'
     def outputShell = readFile('shellOutput').trim().toString()
     print(outputShell)
-    List<String> list = outputShell.asList(str.split("\\s*,\\s*"))
+    List<String> list = Arrays.asList(outputShell.split("\\s*,\\s*"))
     print(list.class)
     print(list)
 //    def list = outputShell.readLines()
