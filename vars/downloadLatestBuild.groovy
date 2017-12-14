87b95f9bf413d13a6e53844ba4fcf0e146a3bf00
vgrys <vgrysster@gmail.com>
2017-12-14 23:22:14 +0200

    tests

--- vars/downloadLatestBuild.groovy
+++ vars/downloadLatestBuild.groovy
@@ -4,7 +4,7 @@ import groovy.json.JsonSlurper
 
 def call(artifactoryId, URL) {
     withCredentials([usernamePassword(credentialsId: "${artifactoryId}", usernameVariable: 'artifactory_user', passwordVariable: 'artifactory_pwd')]) {
-        sh "curl -u ${artifactory_user}:${artifactory_pwd} -X POST  ${URL}/artifactory/api/search/aql -H \"content-type: text/plain\" -d 'items.find({ \"repo\": {\"\$eq\":\"bigdata-dss-automation\"}, \"path\" : \"atf/release\", \"name\": {\"\$match\" : \"atf-*.tar.gz\"}})' > JSON"
+        sh "curl -u ${artifactory_user}:${artifactory_pwd} -X POST  ${URL}/artifactory/api/search/aql -H \"content-type: text/plain\" -d 'items.find({ \"repo\": {\"\$eq\":\"bigdata-dss-automation\"}, \"path\" : \"atf/release\", \"name\": {\"\$match\" : \"atf-*.tar.gz\"}}).sort({\"$asc\" : [\"results\",\"updated\"]})' > JSON"
         sh "cat JSON"
         def JSON = readFile('JSON').trim()
         def output = new JsonSlurper().parseText(JSON)
@@ -15,7 +15,7 @@ def call(artifactoryId, URL) {
 //                    def id = sortedJSON[0].id
         }
 
-        def fileToDownload = "${URL}/artifactory/bigdata-dss-automation/${latestFile}"
+//        def fileToDownload = "${URL}/artifactory/bigdata-dss-automation/${latestFile}"
     }
 //        Use ./jq to pars JSON
 //latestFile=$(echo ${resultAsJson} |  jq -r '.results | sort_by("updated") [-1].name')
