9f59e51fc0d784b3643f4e0343da4194bcff6a8b
vgrys <vgrysster@gmail.com>
2017-12-14 23:09:46 +0200

    tests

--- vars/downloadLatestBuild.groovy
+++ vars/downloadLatestBuild.groovy
@@ -6,7 +6,8 @@ def call(artifactoryId, URL) {
     withCredentials([usernamePassword(credentialsId: "${artifactoryId}", usernameVariable: 'artifactory_user', passwordVariable: 'artifactory_pwd')]) {
         sh "curl -u ${artifactory_user}:${artifactory_pwd} -X POST  ${URL}/artifactory/api/search/aql -H \"content-type: text/plain\" -d 'items.find({ \"repo\": {\"\$eq\":\"bigdata-dss-automation\"}, \"path\" : \"atf/release\", \"name\": {\"\$match\" : \"atf-*.tar.gz\"}})' > output"
         sh "cat output"
-        def result = new JsonSlurper().parseText("output")
+        def output = readFile('JSON').trim()
+        def result = new JsonSlurper().parseText(output)
         def sortedJSON = result.sort { a,b -> b.results.updated <=> a.results.updated}
 
         def id = sortedJSON[0].id
