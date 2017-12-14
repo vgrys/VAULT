aeddc8c82ec9e0d76057df704d204446866efe9e
vgrys <vgrysster@gmail.com>
2017-12-14 23:06:39 +0200

    tests

--- vars/downloadLatestBuild.groovy
+++ vars/downloadLatestBuild.groovy
@@ -1,5 +1,7 @@
 #!/usr/bin/groovy
 
+import groovy.json.JsonSlurper
+
 def call(artifactoryId, URL) {
     withCredentials([usernamePassword(credentialsId: "${artifactoryId}", usernameVariable: 'artifactory_user', passwordVariable: 'artifactory_pwd')]) {
         sh "curl -u ${artifactory_user}:${artifactory_pwd} -X POST  ${URL}/artifactory/api/search/aql -H \"content-type: text/plain\" -d 'items.find({ \"repo\": {\"\$eq\":\"bigdata-dss-automation\"}, \"path\" : \"atf/release\", \"name\": {\"\$match\" : \"atf-*.tar.gz\"}})' > output"
