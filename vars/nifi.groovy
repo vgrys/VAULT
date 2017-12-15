ddcca1853883917b56502c742ab83e913255c998
vgrys <vgrysster@gmail.com>
2017-12-15 17:55:51 +0200

    tests

--- vars/nifi.groovy
+++ vars/nifi.groovy
@@ -61,7 +61,7 @@ def createProcesGroupsAndDeployTemplate(URL, templateMap) {
         echo "I am here"
 //        GString deployTemplate = "'{\"templateId\":\"d29050f0-5627-42f8-b299-e5d8f6a692e7\",\"originX\":-0.0,\"originY\":-0.0}' http://192.168.56.105:8088/nifi-api/process-groups/5ad947e5-0160-1000-e84b-f857cbdd8eef/template-instance"
 //        echo deployTemplate
-        String command = 'curl -H "Content-Type: application/json" -X POST -d \'{"templateId":"d29050f0-5627-42f8-b299-e5d8f6a692e7","originX":-0.0,"originY":-0.0}\' http://192.168.56.105:8088/nifi-api/process-groups/5ad947e5-0160-1000-e84b-f857cbdd8eef/template-instance"'
+        String command = 'curl -H "Content-Type: application/json" -X POST -d \'{"templateId":"d29050f0-5627-42f8-b299-e5d8f6a692e7","originX":-0.0,"originY":-0.0}\' http://192.168.56.105:8088/nifi-api/process-groups/5ad947e5-0160-1000-e84b-f857cbdd8eef/template-instance > /dev/null 2>&1'
         sh command
 //        output = readFile('output').trim()
 //        print(output)
