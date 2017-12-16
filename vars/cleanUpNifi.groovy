2ab9ba5984e0421ab0488a9b1a7118d6a2f1aae9
vgrys <vgrysster@gmail.com>
2017-12-17 01:15:10 +0200

    tests

--- vars/cleanUpNifi.groovy
+++ vars/cleanUpNifi.groovy
@@ -38,11 +38,12 @@ def cleanUpQueue(URL) {
         List connectionsIds = result.processGroupFlow.flow.connections.id
         result = null
         for (List connectionsId : connectionsIds){
-            sh "curl -X POST ${URL}/nifi-api/flowfile-queues/${connectionsId}/drop-requests"
+            sh "curl -X POST ${URL}/nifi-api/flowfile-queues/${connectionsId}/drop-requests > output"
+            def jsonStatus = readFile('output').trim()
+            def status = new JsonSlurper().parseText("${jsonStatus}")
+            echo "State of clean up queue: '${status.dropRequest.state}'"
         }
     }
-//    curl –X POST http://192.168.56.105:8088/nifi-api/flowfile-queues/9e91b008-0f66-3698-8c01-2464d67a19ae/drop-requests
-
 }
 
 def getInfo(URL, process, id) {
