2accef7c9c5d808fdf1554ebac21aaa4b226c5a0
vgrys <vgrysster@gmail.com>
2017-12-16 23:41:33 +0200

    tests

--- vars/nifi.groovy
+++ vars/nifi.groovy
@@ -9,6 +9,7 @@ def call(URL) {
         templateMap = uploadTemplates(URL)
         createWorkspace(URL)
         createProcesGroupsAndDeployTemplate(URL, templateMap)
+        deleteTemplates(URL)
     } catch (err) {
         currentBuild.result = "FAILURE"
         echo "********* Errors happened *********"
@@ -46,7 +47,6 @@ def createWorkspace(URL) {
     env.WORKSPACE_PROCESS_GROUP = result.id
 }
 
-//@NonCPS
 def createProcesGroupsAndDeployTemplate(URL, templateMap) {
 
     List processGroups = []
@@ -63,17 +63,10 @@ def createProcesGroupsAndDeployTemplate(URL, templateMap) {
         String processGroupId = result.id
         processGroups.add(processGroupId)
         result = ''
-        echo "I am here"
-//        sh 'curl -H "Content-Type: application/json" -X POST -d \'{"templateId":"d29050f0-5627-42f8-b299-e5d8f6a692e7","originX":-0.0,"originY":-0.0}\' http://192.168.56.105:8088/nifi-api/process-groups/6124cda9-0160-1000-0de7-30aa81f515d8/template-instance'
-
         GString deployTemplate = "'{\"templateId\":\"${templateId}\",\"originX\":-0.0,\"originY\":-0.0}' ${URL}/nifi-api/process-groups/${processGroupId}/template-instance"
-        echo deployTemplate
         sh "curl -H \"Content-Type: application/json\" -X POST -d ${deployTemplate} > output"
-//      sh 'curl -H "Content-Type: application/json" -X POST -d \'{"templateId":"d29050f0-5627-42f8-b299-e5d8f6a692e7","originX":-0.0,"originY":-0.0}\' http://192.168.56.105:8088/nifi-api/process-groups/5ad947e5-0160-1000-e84b-f857cbdd8eef/template-instance > /dev/null 2>&1'
-//        sh command
 //        output = readFile('output').trim()
 //        print(output)
-        echo "Finished"
 //        template["process_group_id"] = id
 //        echo template
     }
@@ -83,6 +76,12 @@ def createProcesGroupsAndDeployTemplate(URL, templateMap) {
 def deployTemplates() {
     def listId = env.TEMPLATE_ID.toString().split()
     for (String item : listId) {
+    }
+}
+def deleteTemplates(URL) {
+    for (def template in env.TEMPLATE_ID) {
+        echo "template is: ${template}"
+        sh "curl - i - X DELETE ${URL}/nifi-api/templates/${template}"
     }
 }
 
