f10913b24ca8a55942bfdc69567a6c7169caab3d
vgrys <vgrysster@gmail.com>
2017-12-17 23:40:11 +0200

    tests

--- vars/deployNifi.groovy
+++ vars/deployNifi.groovy
@@ -71,6 +71,5 @@ def findTemplates(env) {
     sh "ls -A -m -w 5000 ${env.WORKSPACE}/nifi > output"
     def output = readFile('output').trim().toString().split(", ")
     List list = output
-    print(list)
     return list
 }
