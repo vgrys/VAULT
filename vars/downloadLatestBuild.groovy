8b2cf9f7b5b1c7489f44bfbf1a2fee6268eba1b1
vgrys <vgrysster@gmail.com>
2017-12-18 17:10:07 +0200

    tests

--- vars/downloadLatestBuild.groovy
+++ vars/downloadLatestBuild.groovy
@@ -5,18 +5,20 @@ import groovy.json.JsonSlurper
 class downloadLatestBuild implements Serializable {
 
     def call(artifactoryId, URL, repository, release) {
-        withCredentials([usernamePassword(credentialsId: "${artifactoryId}", usernameVariable: 'artifactory_user', passwordVariable: 'artifactory_pwd')]) {
-            GString command = "\"content-type: text/plain\" -d 'items.find({ \"repo\":{\"\$eq\":\"${repository}\"}, \"path\":\"atf/${release}\", \"name\":{\"\$match\":\"atf-*.tar.gz\"}})' > output"
-            sh "curl -u ${artifactory_user}:${artifactory_pwd} -X POST  ${URL}/artifactory/api/search/aql -H ${command}"
-//        sh "cat output"
-            def output = readFile('output').trim()
-            def result = new JsonSlurper().parseText(output)
-            def max = result.results.updated.max()
-            latestBuild = result.results.find { it.updated == max }
-            result = null
-            max = null
-            print(latestBuild.name)
-            return latestBuild.name
-        }
+        def result = getResult(artifactoryId, URL, repository, release)
+        def max = result.results.updated.max()
+        latestBuild = result.results.find { it.updated == max }
+        print(latestBuild.name)
+        return latestBuild.name
+    }
+}
+
+
+def getResult(artifactoryId, URL, repository, release) {
+    withCredentials([usernamePassword(credentialsId: "${artifactoryId}", usernameVariable: 'artifactory_user', passwordVariable: 'artifactory_pwd')]) {
+        GString command = "\"content-type: text/plain\" -d 'items.find({ \"repo\":{\"\$eq\":\"${repository}\"}, \"path\":\"atf/${release}\", \"name\":{\"\$match\":\"atf-*.tar.gz\"}})' > output"
+        sh "curl -u ${artifactory_user}:${artifactory_pwd} -X POST  ${URL}/artifactory/api/search/aql -H ${command}"
+        def output = readFile('output').trim()
+        return new JsonSlurper().parseText(output)
     }
 }
