2efdcb429e59967b4f47b54c5ef904627eba8a89
vgrys <vgrysster@gmail.com>
2017-12-18 17:00:10 +0200

    tests

--- vars/downloadLatestBuild.groovy
+++ vars/downloadLatestBuild.groovy
@@ -7,11 +7,13 @@ def call(artifactoryId, URL, repository, release) {
     withCredentials([usernamePassword(credentialsId: "${artifactoryId}", usernameVariable: 'artifactory_user', passwordVariable: 'artifactory_pwd')]) {
         GString command = "\"content-type: text/plain\" -d 'items.find({ \"repo\":{\"\$eq\":\"${repository}\"}, \"path\":\"atf/${release}\", \"name\":{\"\$match\":\"atf-*.tar.gz\"}})' > output"
         sh "curl -u ${artifactory_user}:${artifactory_pwd} -X POST  ${URL}/artifactory/api/search/aql -H ${command}"
-        sh "cat output"
+//        sh "cat output"
         def output = readFile('output').trim()
         def result = new JsonSlurper().parseText(output)
         def max = result.results.updated.max()
         latestBuild = result.results.find { it.updated == max }
+        result = null
+        max = null
         print(latestBuild.name)
         return latestBuild.name
     }
