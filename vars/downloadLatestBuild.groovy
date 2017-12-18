4b8092bc40c4ec42f0c0d1e19bf0d82d8abc469d
vgrys <vgrysster@gmail.com>
2017-12-18 16:48:29 +0200

    tests

--- vars/downloadLatestBuild.groovy
+++ vars/downloadLatestBuild.groovy
@@ -2,16 +2,20 @@
 
 import groovy.json.JsonSlurper
 
+
 def call(artifactoryId, URL, repository, release) {
+        def result = getResult(artifactoryId, URL, repository, release)
+        def max = result.results.updated.max()
+        latestBuild = result.results.find{ it.updated == max}
+        return latestBuild.name
+}
+
+def getResult(artifactoryId, URL, repository, release) {
     withCredentials([usernamePassword(credentialsId: "${artifactoryId}", usernameVariable: 'artifactory_user', passwordVariable: 'artifactory_pwd')]) {
         GString command = "\"content-type: text/plain\" -d 'items.find({ \"repo\":{\"\$eq\":\"${repository}\"}, \"path\":\"atf/${release}\", \"name\":{\"\$match\":\"atf-*.tar.gz\"}})' > output"
         sh "curl -u ${artifactory_user}:${artifactory_pwd} -X POST  ${URL}/artifactory/api/search/aql -H ${command}"
         sh "cat output"
         def outputJson = readFile('output').trim()
-        def output = new JsonSlurper().parseText(outputJson)
-        def max = output.results.updated.max()
-        latestBuild = output.results.find{ it.updated == max}
-        output = null
-        return latestBuild.name
+        return new JsonSlurper().parseText(outputJson)
     }
 }
