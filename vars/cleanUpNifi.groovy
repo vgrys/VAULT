4ee341195ea754ac9f721b49b50a779586ac46f7
vgrys <vgrysster@gmail.com>
2017-12-20 14:16:07 +0200

    tests

--- vars/cleanUpNifi.groovy
+++ vars/cleanUpNifi.groovy
@@ -39,7 +39,7 @@ def getConnctionsId(URL) {
         for (connection in connections) {
             echo "I am in second for loop"
             def status = get("-X POST ${URL}/nifi-api/flowfile-queues/${connection.id}/drop-requests")
-//            echo "State of clean up queue: '${status.dropRequest.state}'"
+            echo "State of clean up queue: '${status.dropRequest.state}'"
 //            connectionsId.add(connection.id)
 //            print("connectionsId in loop ${connectionsId}")
         }
