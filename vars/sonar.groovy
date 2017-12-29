93628fbe2b8c5e68bcd68154d933ae2a24822032
vgrys <vgrysster@gmail.com>
2017-12-29 15:12:23 +0200

    tests

--- vars/sonar.groovy
+++ vars/sonar.groovy
@@ -1,14 +1,16 @@
 #!/usr/bin/groovy
 
-def scanner(projectName, sonarUrl, sonarToken, buildId) {
+def scanner(projectName, sonarUrl, buildId) {
     def scannerHome = tool name: 'SonarQube3.0.3'
-    dir("${env.WORKSPACE}") {
-        sh "${scannerHome}/bin/sonar-scanner " +
-            "-Dsonar.projectKey=${projectName} " +
-            "-Dsonar.sources=. " +
-            "-Dsonar.host.url=${sonarUrl}/sonar " +
-            "-Dsonar.login=${sonarToken} " +
-            "-Dsonar.projectName=${projectName} " +
-            "-Dsonar.projectVersion=${buildId} "
+    withCredentials([string(credentialsId: 'sonarTokenId', variable: 'sonarToken')]) {
+        dir("${env.WORKSPACE}") {
+            sh "${scannerHome}/bin/sonar-scanner " +
+                    "-Dsonar.projectKey=${projectName} " +
+                    "-Dsonar.sources=. " +
+                    "-Dsonar.host.url=${sonarUrl}/sonar " +
+                    "-Dsonar.login=${sonarToken} " +
+                    "-Dsonar.projectName=${projectName} " +
+                    "-Dsonar.projectVersion=${buildId} "
+        }
     }
 }
